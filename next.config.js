68d11c3a7a4bcacca0ba0165414eac9828048869
PokerDoDigital <pokerdodigital@gmail.com>
2025-02-10 22:29:36 -0300

    Fix: corrige configuração do Next.js e adiciona _app.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
   images: {
     unoptimized: true,
   },
+  distDir: '.next',
 }
 
 module.exports = nextConfig 
